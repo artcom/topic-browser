35e86007f2477cf562275c45166e589d6188f0ca
tillgi <till.gilsbach@artcom.de>
2018-10-22 17:38:04 +0200

    Adjust legend & corresponding class

--- src/components/legend.js
+++ src/components/legend.js
@@ -2,12 +2,12 @@ import React from "react"
 
 export default function Legend() {
   return (
-    <ul className="tip-color">
+    <ul className="tip">
       <li>
         Use double-click to edit payload.
       </li>
       <li>
-        <kbd>&#x21E7;</kbd> + <kbd>&#x23ce;</kbd> to publish and <kbd>esc</kbd> to cancel.
+        <i>[SHIFT]</i> + <i>[ENTER]</i> to publish and <i>[ESC]</i> to cancel.
       </li>
       <li>
         Use <kbd>/</kbd> in the topic field to create subtopics, e.g. foo/bar.
