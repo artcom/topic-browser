8cd0ac06442e352fe33cbb58731d8dfdb74c4ffb
tillgi <till.gilsbach@artcom.de>
2018-10-22 18:06:14 +0200

    Adjust 'hints' formatting

--- src/components/legend.js
+++ src/components/legend.js
@@ -4,13 +4,13 @@ export default function Legend() {
   return (
     <ul className="tip">
       <li>
-        Use double-click to edit payload.
+        Use <b>double-click</b> to edit <i>payload</i>.
       </li>
       <li>
-        <i>[SHIFT]</i> + <i>[ENTER]</i> to publish and <i>[ESC]</i> to cancel.
+        <b>[SHIFT]</b> + <b>[ENTER]</b> to publish and <b>[ESC]</b> to cancel.
       </li>
       <li>
-        Use <kbd>/</kbd> in the topic field to create subtopics, e.g. foo/bar.
+        Use <kbd>/</kbd> in the topic field to create subtopics, e.g. <i>foo/bar</i>.
       </li>
     </ul>
   )
