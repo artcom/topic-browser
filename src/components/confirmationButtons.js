81c02d8ff775a1cc56f324a75d0bf0cc0c3f9d76
tillgi <till.gilsbach@artcom.de>
2018-10-23 12:25:25 +0200

    Switch button order

--- src/components/confirmationButtons.js
+++ src/components/confirmationButtons.js
@@ -3,8 +3,8 @@ import React from "react"
 export default function ConfirmationButtons({ onConfirm, onCancel }) {
   return (
     <span className="confirm-cancel">
-      <div className="pointer icon-cancel" onClick={ onCancel } />
       <div className="pointer icon-confirm" onClick={ onConfirm } />
+      <div className="pointer icon-cancel" onClick={ onCancel } />
     </span>
   )
 }
